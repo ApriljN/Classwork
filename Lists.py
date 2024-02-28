5993acf1e4688b612de930db9f6ebb19285ce5e0
ApriljN <158239385+ApriljN@users.noreply.github.com>
2024-02-28 20:10:51 +1300

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,7 @@
+# Task1 from google classroom
+# by April Nanai
+# 28/02/2023
+
+marvel = ["spiderman", "Venom", "Wolverine", "Captain A", "Hulk", "Cyclops"]
+capcom = ["Ryu", "Zangief", "Megaman", "Chun-Li", "Morrigan"]
+print(capcom[0] + " Vs " + marvel[1])
